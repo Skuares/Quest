e91e645c1575b223f7d48de324f59bb6059c5553
fakhouri-junior <ceaserfs3@gmail.com>
2015-12-25 20:36:14 +0800

    Adjusting Fragment Todos3

--- app/src/main/java/com/skuares/studio/quest/TodoFragment.java
+++ app/src/main/java/com/skuares/studio/quest/TodoFragment.java
@@ -1,7 +1,9 @@
 package com.skuares.studio.quest;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -33,16 +35,35 @@ public class TodoFragment extends Fragment  {
     ListView listView;
 
 
+    /*
+  * onAttach(Context) is not called on pre API 23 versions of Android and onAttach(Activity) is deprecated
+  * Use onAttachToContext instead
+  */
+    @TargetApi(23)
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        onAttachToContext(context);
     }
 
+    /*
+     * Deprecated on API 23
+     * Use onAttachToContext instead
+     */
+    @SuppressWarnings("deprecation")
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            onAttachToContext(activity);
+        }
     }
 
+    /*
+     * Called when the fragment attaches to the context
+     */
+    protected void onAttachToContext(Context context) {
+    }
 
     @Nullable
     @Override
